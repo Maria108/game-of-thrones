3e155b2859ed5fde6f3394276ef194487d832b10
Maria108 <maria.abash@gmail.com>
2019-05-22 12:57:06 -0400

    Updates

---
+++ precache-manifest.367ac613705dd00323d99290b07b67ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502b63dcf08ccab1df5cd2694c382114",
+    "url": "/Maria108/game-of-thrones/index.html"
+  },
+  {
+    "revision": "4e68e11b3456e1776ae1",
+    "url": "/Maria108/game-of-thrones/static/css/main.54a96270.chunk.css"
+  },
+  {
+    "revision": "4b40f8dc4a8a04c207fd",
+    "url": "/Maria108/game-of-thrones/static/js/2.c9cbea6f.chunk.js"
+  },
+  {
+    "revision": "4e68e11b3456e1776ae1",
+    "url": "/Maria108/game-of-thrones/static/js/main.0a2c9b55.chunk.js"
+  },
+  {
+    "revision": "557315b16554c5b2048b",
+    "url": "/Maria108/game-of-thrones/static/js/runtime~main.f2b617d4.js"
+  },
+  {
+    "revision": "74ac9c2e67a7fc7ce0d395223b87e536",
+    "url": "/Maria108/game-of-thrones/static/media/main.74ac9c2e.jpg"
+  },
+  {
+    "revision": "e5230b8fa178a367974e5fc5def8aba2",
+    "url": "/Maria108/game-of-thrones/static/media/map.e5230b8f.jpeg"
+  },
+  {
+    "revision": "61b3d6faf9f8fa3ff9579ac5bd5dfac4",
+    "url": "/Maria108/game-of-thrones/static/media/white.61b3d6fa.jpg"
+  },
+  {
+    "revision": "d3d68f9234a56d4ba6fe4ab77c93f4a1",
+    "url": "/Maria108/game-of-thrones/static/media/wolf.d3d68f92.jpg"
+  }
+]);
